046ebe19b3863d666f6c765722cbef6b18a17ec8
masukomi <masukomi@masukomi.org>
2026-01-01 08:34:27 -0500

    [git-com] updated to v1.1.0

--- Formula/git-com.rb
+++ Formula/git-com.rb
@@ -1,9 +1,9 @@
 class GitCom < Formula
   desc "CLI tool that creates consistent git commit messages through YAML-configured interactive prompts"
   homepage "https://github.com/masukomi/git-com"
-  current_version="1.0.0"
+  current_version="1.1.0"
   url "https://github.com/masukomi/git-com/archive/refs/tags/v#{current_version}.tar.gz"
-  sha256 "84cd74a4e415c85b3cd6f5df0bb6dff2f277a86194e49856b5c29eaf0d0f90a5"
+  sha256 "83fdf0d8090dd039dcfe9a49275ef260e8fcca134bd2d38c1ebc9aa0f7be14bb"
   license "MIT"
 
   depends_on "go" => :build
